501288ff2e873b380085fb7a5abb632dcab25fa4
khagerman <71734063+khagerman@users.noreply.github.com>
2021-05-21 14:17:35 -0500

    trying to debug heroku

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
     "DATABASE_URL", "postgresql://auth"
 )
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "hellosecret1")
