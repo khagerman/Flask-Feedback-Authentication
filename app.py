034bdaad221489644fa6e5efaf722d9027c3a2fb
khagerman <71734063+khagerman@users.noreply.github.com>
2021-05-21 14:21:38 -0500

    fixed error

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
     "DATABASE_URL", "postgresql://auth"
 )
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
